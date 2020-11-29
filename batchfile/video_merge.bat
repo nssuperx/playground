2bc5f249f248b234070f9e6ace98f7613402680f
nssuperx <mgtmgtx@gmail.com>
2020-11-29 16:16:02 +0900

    add: 動画結合 mv: vfr2cfrをbatchfileに持ってきた

---
+++ batchfile/video_merge.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+cd /d %~dp1
+
+set originalFileName=%~n1
+set outFilePath=%originalFileName%-out.mp4
+
+type nul > tmp.txt
+
+for %%a in (%*) do (
+echo file '%%~nxa' >> tmp.txt
+)
+
+ffmpeg -safe 0 -f concat -i tmp.txt -vcodec copy -acodec copy %outFilePath%
+rem ffmpeg -safe 0 -f concat -i tmp.txt -vcodec copy -acodec copy out.mp4
+
+del tmp.txt
+
+pause
