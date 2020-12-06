d0d1908f047f015530a9d4ba28cb06114908e032
nssuperx <mgtmgtx@gmail.com>
2020-12-06 23:54:00 +0900

    add: 共用体使ってみる

---
+++ cpp/dokushu/union.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+struct S
+{
+    int a;
+    int b;
+    int c;
+};
+
+union U
+{
+    int a;
+    int b;
+    int c;
+};
+
+enum class E
+{
+    value1,
+    value2,
+    value3,
+};
+
+int main(){
+    
+    return 0;
+}
