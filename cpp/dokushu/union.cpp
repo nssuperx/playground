c19be5961f7674aa623be1423c35882d1e6b57c3
nssuperx <mgtmgtx@gmail.com>
2020-12-07 00:27:17 +0900

    update: structとunionで遊んだ

--- cpp/dokushu/union.cpp
+++ cpp/dokushu/union.cpp
@@ -4,18 +4,25 @@ using namespace std;
 struct S
 {
     int a;
-    int b;
+    int32_t b;
     int c;
 };
 
 union U
 {
     int a;
-    int b;
+    char b;
     int c;
 };
 
-enum class E
+enum E
+{
+    value1,
+    value2,
+    value3
+};
+
+enum class Ec
 {
     value1,
     value2,
@@ -23,6 +30,33 @@ enum class E
 };
 
 int main(){
+    S s;
+    U u;
+    E e = value1;
+    Ec ec = Ec::value1;
     
+    s.a = 1;
+    s.b = 2;
+    s.c = 3;
+    cout << "s.a:" << s.a << endl;
+    cout << "s.b:" << s.b << endl;
+    cout << "s.c:" << s.c << endl;
+    cout << "&s.a:" << &s.a << endl;
+    cout << "&s.b:" << &s.b << endl;
+    cout << "&s.c:" << &s.c << endl;
+
+    u.a = 1;
+    cout << "u.a:" << u.a << endl;
+    cout << "u.b:" << u.b << endl;
+    cout << "u.c:" << u.c << endl;
+
+    u.b = 'a';
+    cout << "u.a:" << u.a << endl;
+    cout << "u.b:" << u.b << endl;
+    cout << "u.c:" << u.c << endl;
+
+    cout << "&u.a:" << &u.a << endl;
+    cout << "&u.b:" << &u.b << endl;
+    cout << "&u.c:" << &u.c << endl;
     return 0;
 }
